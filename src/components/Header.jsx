71cd38c19ccf2814a3c75009007c10a2e35dcaf1
abhishek <abhishekpl8219@gmail.com>
2024-04-12 22:52:58 +0530

    Adding image upload functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,71 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import Modal from 'react-modal';
 import { IoMdAddCircleOutline } from 'react-icons/io';
 import { signIn, useSession, signOut } from 'next-auth/react';
 import { HiCamera } from 'react-icons/hi';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from 'firebase/storage';
+
 import { AiOutlineClose } from 'react-icons/ai';
+import { app1 } from '@/firebase';
 export default function Header() {
-     const {data:session}=useSession();
-     console.log(session)
-     const[isOpen,setIsOpen]=useState(false);
-   
+    const { data: session } = useSession();
+    console.log(session)
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const filePickerRef = useRef(null);
+    function addImageToPost(e) {
+        const file = e.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+
+        }
+    }
+
+    useEffect(() => {
+        if (selectedFile) {
+            uploadImageToStorage();
+        }
+
+    }, [selectedFile])
+    async function uploadImageToStorage() {
+        setImageFileUploading(true);
+        const storage = getStorage(app1);
+        const fileName = new Date().getTime() + '-' + selectedFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+            },
+            (error) => {
+                console.error(error);
+                setImageFileUploading(false);
+                setImageFileUrl(null);
+                setSelectedFile(null);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setImageFileUrl(downloadURL);
+                    setImageFileUploading(false);
+                });
+            }
+        );
+    }
+
     return (
         <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
             <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -21,24 +75,30 @@ export default function Header() {
                 <Link href="/" ></Link>
                 <Image src="/800px-Instagram_logo_2016.webp" width={40} height={40} alt='instagram logo' className=' lg:hidden'></Image>
                 <input type='text' placeholder='search' className='bg-gray-50 border border-gray-200 rounded-md w-full text-sm py-2 px-4 max-w-[210px]' />
-                {session?( 
+                {session ? (
                     <div className='flex items-center gap-2'>
-                        <IoMdAddCircleOutline  className='text-2xl cursor-pointer transform hover:scale-125 hover:text-red-500  transition duration-300' onClick={()=>setIsOpen(true)}/>
-                <img src = {session.user.image} alt={session.user.name} onClick={signOut} className='h-10 w-10 rounded-full cursor-pointer' /> </div>):(<button className='text-sm  font-semibold text-blue-500' onClick={signIn} >Log In</button>)}
-                
+                        <IoMdAddCircleOutline className='text-2xl cursor-pointer transform hover:scale-125 hover:text-red-500  transition duration-300' onClick={() => setIsOpen(true)} />
+                        <img src={session.user.image} alt={session.user.name} onClick={signOut} className='h-10 w-10 rounded-full cursor-pointer' /> </div>) : (<button className='text-sm  font-semibold text-blue-500' onClick={signIn} >Log In</button>)}
+
             </div>
             {isOpen && (
-                <Modal isOpen={isOpen} onRequestClose={()=>setIsOpen(false)} ariaHideApp={false}
-                className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] border-2 rounded-md shadow-md bg-white translate-x-[-50%]'>
+                <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} ariaHideApp={false}
+                    className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] border-2 rounded-md shadow-md bg-white translate-x-[-50%]'>
                     <div className='flex flex-col justify-center items-center h-[100%]'>
-                    <HiCamera className='text-5xl text-slate-400 cursor-pointer '></HiCamera>
+
+                        {selectedFile ? (
+                            <img onClick={() => setSelectedFile(null)} src={imageFileUrl} alt="selected file" className={`w-full max-h-[250px] object-over cursor-pointer ${imageFileUploading ? 'animate-pulse' : ''
+                                }`} ></img>
+                        ) : (<HiCamera className='text-5xl text-slate-400 cursor-pointer ' onClick={() => filePickerRef.current.click()} />
+                        )}
+                        <input type='file' hidden accept='image/*' ref={filePickerRef} onChange={addImageToPost} />
                     </div>
                     <input type='text' placeholder='Please enter your caption' maxLength='150' className='border-none m-4 text-center  w-full focus:ring-0 outline-none' />
                     <button disabled className='w-full bg-red-600 text-white p-2 shadow-lg rounded-lg hover:brightness-100 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'>
                         Upload Post
                     </button>
-                    
-                    <AiOutlineClose className='top-2 absolute right-2 cursor-pointer hover:text-red-600 transition duration-300' onClick={()=>setIsOpen(false)}/>
+
+                    <AiOutlineClose className='top-2 absolute right-2 cursor-pointer hover:text-red-600 transition duration-300' onClick={() => setIsOpen(false)} />
 
                 </Modal>
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWvF8uOHPYRNdA6n0gzEWh07rh0vntqs8",
+  authDomain: "instaclone-231d4.firebaseapp.com",
+  projectId: "instaclone-231d4",
+  storageBucket: "instaclone-231d4.appspot.com",
+  messagingSenderId: "473846673553",
+  appId: "1:473846673553:web:0d4ead7a37929869abcf30"
+};
+
+// Initialize Firebase
+export const app1 = initializeApp(firebaseConfig);
