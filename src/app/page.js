aea31f3765b6e2101e74f539410bb297bb4e468b
abhishek <abhishekpl8219@gmail.com>
2024-04-13 08:32:32 +0530

    Make the feed responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Feed from '@/components/Feed'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className='text-red-400'>
-      Home
-    </div>
+<>
+<Feed />
+</>
   )
 }
