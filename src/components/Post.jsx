c5527a76b372bfdf4c17342e6760859417d66343
abhishek <abhishekpl8219@gmail.com>
2024-04-13 13:21:31 +0530

    added the like section

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { HiOutlineDotsVertical } from 'react-icons/hi';
+import LikeSection from './LikeSection';
 
 export default function Post({post}) {
     console.log(post)
@@ -11,6 +12,7 @@ export default function Post({post}) {
         <HiOutlineDotsVertical className='h-5 cursor-pointer'></HiOutlineDotsVertical>
      </div>
      <img src = {post.image} alt={post.caption} className='object-cover w-full' />
+     <LikeSection id = {post.id}/>
      <p className='p-5 truncate'>
         <span className='font-bold mr-2'>
             {post.username}
