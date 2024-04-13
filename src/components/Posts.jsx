58e274276fd4d4dc132ad744f0d9382a9eec3e54
abhishek <abhishekpl8219@gmail.com>
2024-04-13 11:48:54 +0530

    Complete the post card

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {
+    collection,
+    getDocs,
+    getFirestore,
+    orderBy,
+    query,
+  } from 'firebase/firestore';
+import { app1 } from '../firebase';
+import Post from './Post';
+export default async function Posts() {
+    const db = getFirestore(app1);
+    const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
 
-export default function Posts() {
   return (
     <div>
-      Posts
-    </div>
+    {data.map((post) => (
+      <Post key={post.id} post={post} />
+    ))}
+  </div>
   )
 }
